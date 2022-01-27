751018fc4d328007ad9b170bb0da4fe1d552bf9e
configtheworld <erkinture@gmail.com>
2022-01-28 01:45:50 +0300

    default zoom and marker animation added

--- src/components/MapComponent/MapComponent.js
+++ src/components/MapComponent/MapComponent.js
@@ -9,12 +9,13 @@ import {
 const Map = withScriptjs(
   withGoogleMap((props) => (
     <GoogleMap
-      defaultZoom={12}
+      defaultZoom={10}
       defaultCenter={{ lat: 39.9126519, lng: 32.7544914 }}
       onClick={(e) => props.onMapClick(e)}
     >
       {props.marks.map((mark) => (
         <Marker
+          animation={window.google.maps.Animation.DROP}
           key={mark.id}
           position={mark.location}
           title={mark.name + ' - ' + mark.type[0]}
